4716d40b0c1d7f6f8656818cbd3aef7036994893
Parth <parth.tripathi17@gmail.com>
2021-06-14 17:21:08 +0000

    Final Commit

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function compute()
 {
     prinflag = checkPrincipal()
+    if (prinflag==false)
+    {
+        document.getElementById("principal").style.backgroundColor = "#fd5656"
+        document.getElementById("result_val").innerHTML = ""
+        return false;
+    }
     var prin = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var year = document.getElementById("years").value;
-
+    document.getElementById("principal").style.backgroundColor = "#ffffff"
     var interest = prin * year * rate /100;
-    var amount = parseInt(prin) + parseInt(interest)
     var formatted_year = new Date().getFullYear()+parseInt(year);
 
-    document.getElementById("result_val").innerHTML ="If you deposit "+prin+",\<br\>at an interest rate of "+rate+"%<br>You will receive an amount of "+interest+",\<br\>in the year "+formatted_year+"\<br\>"
+    document.getElementById("result_val").innerHTML ="If you deposit \<span\ id = 'highlight'>"+prin+"\</span\>,\<br\>at an interest rate of \<span\ id = 'highlight'>"+rate+"%\</span\><br>You will receive an amount of \<span\ id = 'highlight'>"+interest+"\</span\>,\<br\>in the year \<span\ id = 'highlight'>"+formatted_year+"\</span\>\<br\>"
 }
 
 function updateRate()
@@ -23,8 +28,9 @@ function checkPrincipal()
    var prin = document.getElementById("principal").value;
    if (prin <=0)
    {
-       alert("Enter a positive number");
-       prin.focus();
+        document.getElementById("principal").focus();   
+        alert("Enter a positive number");
+        
        return false;
    }
    return true;
